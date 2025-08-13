6376b84342282d2367890485cb21c38404ef198f
Robert-Apodaca <55193850+Robert-Apodaca@users.noreply.github.com>
2025-08-13 16:43:06 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
 export default {
-	async fetch(request): Promise<Response> {
-		const response = await fetch(request);
+    async fetch(request): Promise<Response> {
+        const response = await fetch(request);
+        const newResponse = new Response(response.body, response);
 
-		// Clone the response so that it's no longer immutable
-		const newResponse = new Response(response.body, response);
+        // Extract the hostname from the request
+        const hostname = new URL(request.url).hostname;
 
-		// Add a custom header with a value
-		newResponse.headers.append(
-			"x-workers-hello",
-			"Hello from Cloudflare Workers",
-		);
+        // Match and transform hostname to corresponding tech domain
+        if (hostname.endsWith("-cst.circasports.com")) {
+            const subdomain = hostname.split("-cst.circasports.com")[0];
+            const mappedDomain = `${subdomain}.circasports-tech.com`;
 
-		// Delete headers
-		newResponse.headers.delete("x-header-to-delete");
-		newResponse.headers.delete("x-header2-to-delete");
+            // Set the custom header
+            newResponse.headers.set("Host", mappedDomain);
+        }
 
-		// Adjust the value for an existing header
-		newResponse.headers.set("x-header-to-change", "NewValue");
+        // Add a custom header
+        // newResponse.headers.append("x-workers-hello", "Hello from Cloudflare Workers");
 
-		return newResponse;
-	},
+        // Delete headers
+        // newResponse.headers.delete("x-header-to-delete");
+        // newResponse.headers.delete("x-header2-to-delete");
+
+        // Adjust the value for an existing header
+        //newResponse.headers.set("x-header-to-change", "NewValue");
+
+        return newResponse;
+    },
 } satisfies ExportedHandler;
